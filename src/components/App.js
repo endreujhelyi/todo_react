f909989df73ee4f07ffe713f7d2f479ed6bb2e06
Endre <endreujhelyi@gmail.com>
2017-01-31 11:42:09 +0100

    create todo app with react

---
+++ src/components/App.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {TodoForm, TodoList} from './todo';
+import {addTodo} from './lib/todoHelpers';
+
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [
+        {id: 1, name: 'Learn JSX', isComplete: true},
+        {id: 2, name: 'Build an awesome App', isComplete: false},
+        {id: 3, name: 'Ship It!', isComplete: false}
+      ],
+      currentTodo: ''
+    };
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleSubmit() {
+    evt
+  }
+
+  handleInputChange (evt) {
+    this.setState({
+      currentTodo: evt.target.value
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>React Todos</h2>
+        </div>
+        <div className="Todo-App">
+          <TodoForm handleInputChange={this.handleInputChange}
+          currentTodo={this.state.currentTodo}
+          handleSubmit={this.handleSubmit} />
+          <TodoList todos={this.state.todos}
+           />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
